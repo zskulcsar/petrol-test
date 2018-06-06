b6ca67b52fedb7543012bfe5d5ed6020bc5f66ac
Zsolt Kulcsar <zsolt.kulcsar@infinityworks.com>
2018-06-06 14:22:43 +0100

    docker export doesn't handle metadata

--- host/shell/docker-setup.sh
+++ host/shell/docker-setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-docker import - zskulcsar/petrol-nginx:latest < /tmp/petrol-nginx.tar
-docker run -d -p 80:80 -p 443:443 zskulcsar/petrol-nginx:latest /bin/true
+# This is unfortunate here: that the host need to know this lot about the container image
+# simple solution would be to use docker repo ...
+docker import - -c 'CMD ["nginx", "-g", "pid /tmp/nginx.pid; daemon off;"]' zskulcsar/petrol-nginx:latest < /tmp/petrol-nginx.tar
+docker run -d -p 80:80 -p 443:443 zskulcsar/petrol-nginx:latest
